7c6d10b53afb55e23752789afd535fc108b65007
Abi Scholz <abi.scholz@gmail.com>
2022-08-12 11:33:55 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fcebcbef34beafec93174c42c324ea2.js"
+  "/csg-catering/precache-manifest.ef1979242e677103d94d7e19d393bc20.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/csg-catering/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
--- src/components/CartItem.js/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const CartItem = (props) => {
-  return <div>{props.children}</div>
-}
-
-export default CartItem
